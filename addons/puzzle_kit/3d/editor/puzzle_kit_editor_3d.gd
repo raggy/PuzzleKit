0d5f6999fe8565861e42b063b5bdf26ad4019b03
Benjamin Davis <518044+raggy@users.noreply.github.com>
2026-01-27 13:46:50 +0000

    Generated piece palette

--- addons/puzzle_kit/3d/editor/puzzle_kit_editor_3d.gd
+++ addons/puzzle_kit/3d/editor/puzzle_kit_editor_3d.gd
@@ -2,13 +2,13 @@
 class_name PuzzleKitEditor3D
 extends VBoxContainer
 
+@export var palette: ItemList
+
 var _board: Board3D
 
 var _debug_material: StandardMaterial3D
 var _debug_mesh: ArrayMesh
 var _debug_mesh_instance: MeshInstance3D
-
-var palette: FlowContainer
 
 func _enter_tree() -> void:
     _debug_material = StandardMaterial3D.new()
@@ -29,11 +29,36 @@ func edit(board: Board3D) -> void:
     _board = board
     if not _board:
         return
-    _build_palette()
+    _update_palette()
 
-func _build_palette() -> void:
-    if not _board:
-        return
+func _update_palette() -> void:
+    palette.clear()
+
+    _add_to_palette_from_dir("res://")
+
+func _add_to_palette_from_dir(path: String) -> void:
+    for filename in DirAccess.get_files_at(path):
+        if not filename.ends_with(".tscn"):
+            # Only interested in scenes
+            continue
+        var file_path := path.path_join(filename)
+        var packed_scene := load(file_path) as PackedScene
+        if not packed_scene:
+            # Failed to load scene
+            continue
+        var scene_state := packed_scene.get_state()
+        if scene_state.get_node_type(0) != "Node3D":
+            # Only interested in scenes with Node3D root nodes
+            continue
+        EditorInterface.get_resource_previewer().queue_resource_preview(file_path, self, "_add_palette_item", null)
+    for dir_name in DirAccess.get_directories_at(path):
+        if dir_name.begins_with("."):
+            # Ignore hidden directories
+            continue
+        _add_to_palette_from_dir(path.path_join(dir_name))
+
+func _add_palette_item(path: String, preview: Texture2D, _thumbnail_preview: Texture2D, _userdata: Variant) -> void:
+    palette.add_item(path, preview)
 
 func preview_raycast(from: Vector3, direction: Vector3) -> void:
     _debug_mesh.clear_surfaces()
--- addons/puzzle_kit/3d/editor/puzzle_kit_editor_3d_plugin.gd
+++ addons/puzzle_kit/3d/editor/puzzle_kit_editor_3d_plugin.gd
@@ -1,6 +1,7 @@
 @tool
 extends EditorPlugin
 
+var _editor_scene := load("res://addons/puzzle_kit/3d/editor/puzzle_kit_editor_3d.tscn") as PackedScene
 var _editor: PuzzleKitEditor3D
 var _editor_button: Button
 
@@ -11,7 +12,7 @@ func _disable_plugin() -> void:
     pass
 
 func _enter_tree() -> void:
-    _editor = PuzzleKitEditor3D.new()
+    _editor = _editor_scene.instantiate()
     _editor_button = add_control_to_bottom_panel(_editor, "Board3D")
     _editor_button.hide()
 
@@ -23,11 +24,6 @@ func _exit_tree() -> void:
 
 func _edit(object: Object) -> void:
     _editor.edit(object as Board3D)
-
-    if object:
-        _editor_button.show()
-    else:
-        _editor_button.hide()
 
 func _handles(object: Object) -> bool:
     return object is Board3D
@@ -42,3 +38,14 @@ func _forward_3d_gui_input(viewport_camera: Camera3D, event: InputEvent) -> int:
             print(_editor._board.raycast_piece(viewport_camera.project_ray_origin(button_event.position), viewport_camera.project_ray_normal(button_event.position)))
             return EditorPlugin.AFTER_GUI_INPUT_STOP
     return EditorPlugin.AFTER_GUI_INPUT_PASS
+
+func _make_visible(visible: bool) -> void:
+    if visible:
+        _editor_button.show()
+        make_bottom_panel_item_visible(_editor)
+        _editor.set_process(true)
+    else:
+        _editor_button.hide()
+        if _editor.is_visible_in_tree():
+            hide_bottom_panel()
+        _editor.set_process(false)
