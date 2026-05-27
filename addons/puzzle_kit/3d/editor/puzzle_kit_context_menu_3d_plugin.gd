477620e985669ccfc6e8b5d699c08f5c07cc1295
Benjamin Davis <518044+raggy@users.noreply.github.com>
2026-05-27 14:33:38 +0100

    Add quick context menu for testing editing sub-boards

---
+++ addons/puzzle_kit/3d/editor/puzzle_kit_context_menu_3d_plugin.gd
@@ -0,0 +1,21 @@
+class_name PuzzleKitContextMenu3DPlugin
+extends EditorContextMenuPlugin
+
+var editor: PuzzleKitEditor3D
+
+func _popup_menu(paths: PackedStringArray) -> void:
+    if paths.size() != 1:
+        return
+    
+    var edited_scene_root := EditorInterface.get_edited_scene_root()
+    var selected_node := edited_scene_root.get_node(paths[0])
+    if selected_node is Board3D:
+        add_context_menu_item("Edit with PuzzleKit", edit_board)
+
+func edit_board(args: Array) -> void:
+    if args.size() != 1:
+        return
+    
+    if args[0] is Board3D:
+        var board: Board3D = args[0]
+        editor.edit(board)
--- addons/puzzle_kit/3d/editor/puzzle_kit_editor_3d_plugin.gd
+++ addons/puzzle_kit/3d/editor/puzzle_kit_editor_3d_plugin.gd
@@ -5,6 +5,8 @@ var _editor_scene := load("res://addons/puzzle_kit/3d/editor/puzzle_kit_editor_3
 var _editor: PuzzleKitEditor3D
 var _editor_button: Button
 
+var _context_menu: PuzzleKitContextMenu3DPlugin
+
 func _enable_plugin() -> void:
     pass
 
@@ -18,11 +20,18 @@ func _enter_tree() -> void:
     _editor.undo_redo = get_undo_redo()
     _editor_button = add_control_to_bottom_panel(_editor, "PuzzleKit")
 
+    _context_menu = PuzzleKitContextMenu3DPlugin.new()
+    _context_menu.editor = _editor
+    add_context_menu_plugin(EditorContextMenuPlugin.CONTEXT_SLOT_SCENE_TREE, _context_menu)
+
 func _exit_tree() -> void:
     remove_control_from_bottom_panel(_editor)
     _editor.queue_free()
     _editor = null
     _editor_button = null
+
+    remove_context_menu_plugin(_context_menu)
+    _context_menu = null
 
 func _forward_3d_gui_input(viewport_camera: Camera3D, event: InputEvent) -> int:
     return _editor.forward_spatial_input_event(viewport_camera, event)
